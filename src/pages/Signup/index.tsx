8c3ede397f119617c01dd63ced112daa3bdc193d
Kim-HyounSuk <kkoor586@naver.com>
2024-08-30 09:11:21 +0900

    Feat: route 설정

---
+++ src/pages/Signup/index.tsx
@@ -0,0 +1,5 @@
+const Signup = () => {
+	return <div>Signup 페이지</div>;
+};
+
+export default Signup;
---
+++ src/router.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+import App from '@/App';
+import Login from '@pages/Login';
+import Signup from './pages/Signup';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				path: '/',
+				element: <Navigate to="/login" />,
+			},
+			{
+				path: 'login',
+				element: <Login />,
+			},
+			{
+				path: 'signup',
+				element: <Signup />,
+			},
+		],
+	},
+]);
+
+export default router;
